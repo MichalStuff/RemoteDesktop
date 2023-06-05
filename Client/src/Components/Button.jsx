af4c94a10982ac41fe2c9c9d24bfc7a7a051304c
Michał Szuleta <mszuleta98@gmail.com>
2023-06-06 01:22:48 +0200

    refactor: change menu and button style

--- Client/src/Components/Button.jsx
+++ Client/src/Components/Button.jsx
@@ -1,7 +1,13 @@
 import styled from "styled-components";
 
 const StyledButton = styled.button`
-  background-color: white;
+  background-image: url(${(props) => props.icon});
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: contain;
+  background-color: transparent;
+  width: 70px;
+  // heigh: 70px;
   margin: 10px;
   padding: 10px;
   color: black;
@@ -10,6 +16,8 @@ const StyledButton = styled.button`
 export const Button = (props) => {
   return (
     <StyledButton
+      className={props.className}
+      icon={props.icon}
       onClick={(e) => {
         props.handler(e);
       }}
--- Client/src/Components/Menu.jsx
+++ Client/src/Components/Menu.jsx
@@ -6,50 +6,66 @@ import { Button } from "./Button";
 import { useEffect } from "react";
 import { useState } from "react";
 
+import DesktopLogo from "../assets/desktop.svg";
+
 const StyledMenu = styled.nav`
+  position: absolute;
+  margin: 0 auto;
+  top: 0;
+  left 50%;
+  transform : translateX(-50%);
   display: flex;
   width: 90%;
   height: 8vh;
-  background-color: red;
-  margin: 30px;
-  border-radius: 10px;
+  background: rgba(255, 255, 255, 0.15);
+  border-radius: 16px;
+  box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+  backdrop-filter: blur(7.6px);
+  -webkit-backdrop-filter: blur(7.6px);
 `;
 
 const Menu = () => {
-  let intervalId;
+  // let intervalId;
 
   const [displays, setDisplays] = useState([]);
+  const [streamStatus, setStreamStatus] = useState(false);
 
   useEffect(() => {
     console.log(displays);
   }, [displays]);
+
+  useEffect(() => {
+    let intervalId;
+    if (streamStatus !== false) {
+      intervalId = setInterval(() => {
+        console.log("Streaming");
+        takeScreenShot();
+      }, 100);
+    }
+    return () => {
+      console.log("Stop");
+      clearInterval(intervalId);
+    };
+  }, [streamStatus]);
 
   const ShowDisplays = () => {
     return displays.map((display, index) => {
       return (
         <Button
           key={index}
+          icon={DesktopLogo}
           handler={() => {
-            // socket.emit("ChangeDisplay", display.id);
             socket.emit("ChangeDisplay", index);
           }}
         >
-          Display {index + 1}
+          {index + 1}
         </Button>
       );
     });
   };
 
-  const startStream = () => {
-    //Stream loop
-    intervalId = setInterval(() => {
-      takeScreenShot();
-    }, 100);
-  };
-
-  const stopStream = () => {
-    console.log("Stop");
-    clearInterval(intervalId);
+  const handleStream = () => {
+    setStreamStatus((prev) => !prev);
   };
 
   const takeScreenShot = () => {
@@ -68,8 +84,9 @@ const Menu = () => {
 
   return (
     <StyledMenu>
-      <Button handler={startStream}>Start Stream</Button>
-      <Button handler={stopStream}>Stop Stream</Button>
+      <Button handler={handleStream}>
+        {streamStatus ? "Stop Stream" : "Start Stream"}
+      </Button>
       {ShowDisplays()}
     </StyledMenu>
   );
